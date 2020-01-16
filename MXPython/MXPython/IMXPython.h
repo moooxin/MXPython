bc04cd98668b09dc8b3791cb709380bf9e10d3fc
loki <lihp@fsmeeting.com>
2020-01-16 17:08:58 +0800

    注释说明

--- MXPython/MXPython/IMXPython.h
+++ MXPython/MXPython/IMXPython.h
@@ -31,6 +31,36 @@ namespace mxpy
     };
 #endif
 
+    //ExcuteMethod 中 paramFormat 以 , 分割。格式化的类型翻译如下----------------------------------------
+    //():tuple
+    //[]:list
+    //{}:dict
+    //b:
+    //B:
+    //h:
+    //i:int
+    //H:unsigned in
+    //I:long from unsigned int
+    //n:ling from size_t
+    //l:long ftom long
+    //k:long from unsigned long
+    //L:long from long long
+    //K:long from unsigned long long
+    //u:unicode fromt wide char
+    //f:
+    //d:float from double
+    //D:complex from c complex
+    //c:bytes from string[0]
+    //C:unicode
+    //s:
+    //z:
+    //U:string
+    //y:bytes
+    //N:
+    //S:
+    //O:object
+    //----------------------------------------------------------------------------------------------------
+
     struct IMXPythonUtil
     {
         virtual mxtoolkit::Result ReleaseString(char* buffer) = 0;
